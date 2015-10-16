a204d7af30219b3f4663edea1118efda6e141bf0
Jackie Ng <jumpinjackie@gmail.com>
2015-10-16 21:31:07 +1100

    Initial commit

---
+++ CesiumAspNetDemo/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CesiumAspNetDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hello3DWorld()
+        {
+            return View();
+        }
+    }
+}
